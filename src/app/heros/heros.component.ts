f50e7b6a42837e3f536941e1db56897c8feef694
Keith Compton <comptonk15@gmail.com>
2018-03-30 08:59:25 -0500

    Added a hero service and css styling.

--- src/app/heros/heros.component.ts
+++ src/app/heros/heros.component.ts
@@ -1,16 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROS } from '../mock.heros';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heros.component.html',
   styleUrls: ['./heros.component.css']
 })
+
 export class HerosComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  heros = HEROS;
+
+  selectedHero: Hero;
 
   constructor() { 
 
@@ -19,4 +20,9 @@ export class HerosComponent implements OnInit {
   ngOnInit() {
 
   }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+  
 }
